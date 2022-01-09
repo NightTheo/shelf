d36a0fddab368d12686015ffdf4303811d943f39
Nigth <omnes.theo@gmail.com>
2022-01-09 20:01:59 +0100

    add typeORM and connect it to mysql container

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,32 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { BooksModule } from './books/books.module';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {BooksModule} from './books/books.module';
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {createConnection} from "typeorm";
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+      ConfigModule.forRoot({isGlobal: true}),
+      TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: (configService: ConfigService) => ({
+              type: 'mysql',
+              host: configService.get('DB_HOST'),
+              port: +configService.get<number>('DB_PORT'),
+              username: configService.get('DB_USERNAME'),
+              password: configService.get('DB_PASSWORD'),
+              database: configService.get('DB_DATABASE'),
+              entities: [__dirname + '/**/*.entity{.ts,.js}']
+          }),
+          connectionFactory: async (options) => {
+              return await createConnection(options);
+          },
+      }),
+      BooksModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
