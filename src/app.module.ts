51f933720c661f2eca15255aa8cef1762d88a46c
Pierre <bonnamy.77@gmail.com>
2022-01-16 14:31:00 +0100

    PR config service DB_TYPE

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {BooksModule} from './books/books.module';
 import {ConfigModule, ConfigService} from "@nestjs/config";
-import {TypeOrmModule} from "@nestjs/typeorm";
+import {TypeOrmModule, TypeOrmModuleOptions} from "@nestjs/typeorm";
 import {createConnection} from "typeorm";
 
 @Module({
@@ -12,15 +12,17 @@ import {createConnection} from "typeorm";
       TypeOrmModule.forRootAsync({
           imports: [ConfigModule],
           inject: [ConfigService],
-          useFactory: (configService: ConfigService) => ({
-              type: 'mysql',
-              host: configService.get('DB_HOST'),
-              port: +configService.get<number>('DB_PORT'),
-              username: configService.get('DB_USERNAME'),
-              password: configService.get('DB_PASSWORD'),
-              database: configService.get('DB_DATABASE'),
-              entities: [__dirname + '/**/*.entity{.ts,.js}']
-          }),
+          useFactory: (configService: ConfigService) => {
+              return {
+                type: configService.get('DB_TYPE'),
+                host: configService.get('DB_HOST'),
+                port: +configService.get<number>('DB_PORT'),
+                username: configService.get('DB_USERNAME'),
+                password: configService.get('DB_PASSWORD'),
+                database: configService.get('DB_DATABASE'),
+                entities: [__dirname + '/**/*.entity{.ts,.js}']
+              } as TypeOrmModuleOptions;
+          },
           connectionFactory: async (options) => {
               return await createConnection(options);
           },
