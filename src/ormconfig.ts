6737bb9cb32a67d5f49994ab48cefcd58c13da20
Nigth <omnes.theo@gmail.com>
2022-01-23 15:15:59 +0100

    create a factory for Typeorm connection options

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -1,7 +1,9 @@
 import {ConnectionOptions} from "typeorm";
+import {MysqlConnectionOptions} from "typeorm/driver/mysql/MysqlConnectionOptions";
+import {PostgresConnectionOptions} from "typeorm/driver/postgres/PostgresConnectionOptions";
 require('dotenv').config();
 
-const ormConfig: ConnectionOptions = {
+const options = {
     type: 'mysql',
     host: process.env.DB_HOST,
     port: +process.env.DB_PORT,
@@ -17,4 +19,13 @@ const ormConfig: ConnectionOptions = {
     cli: {migrationsDir: 'src/migrations'}
 };
 
+function connectionOptionsFactory(type: string): ConnectionOptions {
+    switch (type) {
+        case 'mysql': return options as MysqlConnectionOptions;
+        case 'postgre': return options as PostgresConnectionOptions;
+        default: throw Error('Unhandled database type.');
+    }
+}
+
+const ormConfig: ConnectionOptions = connectionOptionsFactory(process.env.DB_TYPE);
 export = ormConfig;
