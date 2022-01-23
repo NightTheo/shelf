64919f2317ef58689d854e1aa343679b4c9fe327
Nigth <omnes.theo@gmail.com>
2022-01-23 10:12:08 +0100

    change database env by URL

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -3,20 +3,14 @@ require('dotenv').config();
 
 const ormConfig: ConnectionOptions = {
     type: 'mysql',
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    url: process.env.DB_URL,
     synchronize: false,
     dropSchema: false,
     migrationsRun: true,
     logging: true,
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
     migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
-    cli: {
-        migrationsDir: 'src/migrations',
-    }
+    cli: {migrationsDir: 'src/migrations'}
 };
 
 export = ormConfig;
