a792c557749aaedfbbc8a59dd028ae26de19b629
Nigth <omnes.theo@gmail.com>
2022-01-23 15:24:53 +0100

    remove length validation in domain object BookTitle

--- src/books/domain/book-title.ts
+++ src/books/domain/book-title.ts
@@ -1,6 +1,5 @@
 export class BookTitle{
     private readonly _value: string;
-    private readonly maxLength: number = 200;
 
     constructor(title: string) {
         this._value = title;
@@ -9,9 +8,5 @@ export class BookTitle{
 
     get value(): string {
         return this._value;
-    }
-
-    isValid() {
-        return this._value.length <= this.maxLength;
     }
 }
