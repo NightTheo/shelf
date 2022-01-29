88761e7804655f2ae2fc6208f0b85b60b26b346f
Nigth <omnes.theo@gmail.com>
2022-01-29 23:04:33 +0100

    add migration for readCount property

---
+++ src/migrations/1643493624478-AddReadCount.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AddReadCount1643493624478 implements MigrationInterface {
+    name = 'AddReadCount1643493624478'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`book\` ADD \`read_count\` int NULL DEFAULT '1'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE \`book\` DROP COLUMN \`read_count\``);
+    }
+
+}
