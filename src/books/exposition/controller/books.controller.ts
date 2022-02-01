08d80771de14c53e04ae591f2ddc4d7899d71d8b
Nigth <omnes.theo@gmail.com>
2022-02-01 19:53:13 +0100

    add CI

--- src/books/exposition/controller/books.controller.ts
+++ src/books/exposition/controller/books.controller.ts
@@ -8,17 +8,16 @@ import {
   Delete,
   UseFilters,
   HttpCode,
-  NotFoundException
+  NotFoundException,
 } from '@nestjs/common';
 import { BooksService } from '../../application/books.service';
 import { AddBookDto } from '../../dto/add-book.dto';
 import { UpdateBookDto } from '../../dto/update-book.dto';
-import {Book} from "../../domain/book";
-import { Isbn } from 'src/books/domain/isbn';
-import {GetBookDto} from "../../dto/get-book.dto";
-import {BookExceptionFilter} from "../filters/book-exception.filter";
-import {GetBookDtoAdapter} from "../../adapters/get-book-dto.adapter";
-
+import { Book } from '../../domain/book';
+import { Isbn } from '../../domain/isbn';
+import { GetBookDto } from '../../dto/get-book.dto';
+import { BookExceptionFilter } from '../filters/book-exception.filter';
+import { GetBookDtoAdapter } from '../../adapters/get-book-dto.adapter';
 
 @Controller('books')
 export class BooksController {
@@ -27,31 +26,32 @@ export class BooksController {
   @Post()
   @HttpCode(201)
   @UseFilters(new BookExceptionFilter())
-  async add(@Body() addBookDto: AddBookDto): Promise<any>{
+  async add(@Body() addBookDto: AddBookDto): Promise<any> {
     const isbn: string = await this.booksService.add(addBookDto);
     return {
-      isbn: isbn
-    }
+      isbn: isbn,
+    };
   }
 
   @Get()
   async findAll(): Promise<GetBookDto[]> {
-    return (await this.booksService.findAll()).map(book => GetBookDtoAdapter.from(book));
+    return (await this.booksService.findAll()).map((book) =>
+      GetBookDtoAdapter.from(book),
+    );
   }
 
   @Get(':id')
   @UseFilters(new BookExceptionFilter())
-  async findOne(@Param('id') id: string): Promise<GetBookDto>{
-    const isbn = new Isbn(id)
+  async findOne(@Param('id') id: string): Promise<GetBookDto> {
+    const isbn = new Isbn(id);
 
     const book: Book = await this.booksService.findOne(isbn);
 
-    if(book) {
+    if (book) {
       return GetBookDtoAdapter.from(book);
     } else {
       throw new NotFoundException('Book Not Found');
     }
-    
   }
 
   @Patch(':id')
