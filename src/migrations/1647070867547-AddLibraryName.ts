e696061cf83aca632c096ba5dca2fa246592e8e9
Nigth <omnes.theo@gmail.com>
2022-03-12 10:02:49 +0100

    fix prettier format

--- src/migrations/1647070867547-AddLibraryName.ts
+++ src/migrations/1647070867547-AddLibraryName.ts
@@ -1,14 +1,15 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class AddLibraryName1647070867547 implements MigrationInterface {
-    name = 'AddLibraryName1647070867547'
+  name = 'AddLibraryName1647070867547';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`library\` ADD \`name\` varchar(255) NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE \`library\` ADD \`name\` varchar(255) NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE \`library\` DROP COLUMN \`name\``);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE \`library\` DROP COLUMN \`name\``);
+  }
 }
